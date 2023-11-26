36d5cbaccb5e2ad09f04d6512e884f2e8618896c
Emily C Wilke <emilycwilke@gmail.com>
2023-11-26 12:25:13 +0100

    started zip extraction #2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+import zipfile
+import geopandas as gp
+
 def main():
     
     #TODO read in gtfs feed
+    with zipfile.ZipFile('/home/emily/thesis_BA/data/gtfs/2023_rnv_gtfs.zip') as gtfs:
+        with gtfs.open('stops.txt') as stops_file:
+            gp.GeoDataFrame(stops_file)
+
     #TODO get bounds from gtfs feed
     #TODO download relevant osm data within those bounds
     #TODO instantiate TravelTimeMatrixComputer object from r5py
