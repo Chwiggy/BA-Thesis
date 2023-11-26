aa2f25ebc1905c61a78b9457516de1d401379e8d
Emily C Wilke <emilycwilke@gmail.com>
2023-11-26 11:57:01 +0100

    added todos

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 def main():
-    print('hello world!')
+    
+    #TODO read in gtfs feed
+    #TODO get bounds from gtfs feed
+    #TODO download relevant osm data within those bounds
+    #TODO instantiate TravelTimeMatrixComputer object from r5py
+    #TODO find pois    
 
 if __name__ == "__main__":
     main()
