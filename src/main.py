0db7cdcd86e386c2a1dba187e4838ca9f0bfe654
Emily C Wilke <emilycwilke@gmail.com>
2023-12-15 17:22:11 +0100

    hexgrids per county #8

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import zipfile
 import geopandas as gpd
 import pandas as pd
 import r5py
+import pyrosm
+import h3pandas as h3
 from sys import argv
-from osmfile import get_osm_data
+from osmfile import get_osm_data, OSMFile
 
 
 def main():
@@ -25,17 +27,35 @@ def main():
     # TODO config file with save locations
 
     matching_file = get_osm_data(geodata=stops_gdf, name=gtfs_name)
+    #hexgrids and start locations per county in dataframe
+    hexgrids = extract_county_hexgrids(matching_file)
+
+    # TODO find pois and create destination enum
 
     transport_network = r5py.TransportNetwork(
         osm_pbf=matching_file.path, gtfs=gtfs_path
     )
 
-    # TODO hexgrids and start locations
-    # TODO find pois and create destination enum
-
     # TODO instantiate TravelTimeMatrixComputer object from r5py
     # TODO loop over counties with travel time matrix calculations
     # TODO output plots and data files
+
+def extract_county_hexgrids(matching_file: OSMFile) -> dict:
+    osm_data = pyrosm.pyrosm.OSM(matching_file.path)
+    admin_boundaries = osm_data.get_boundaries()
+    # TODO add admin_levels enum for different countries
+    counties = admin_boundaries[admin_boundaries['admin_level'] == '6']
+
+    hexgrid = counties.h3.polyfill_resample(10)
+    hexgrid.rename(columns={"id":"county_id", "h3_polyfill":"id"}, inplace=True)
+    start_loc_per_county = {}
+    for name in counties['name']:
+        county_hexgrid = hexgrid.loc[hexgrid['name'] == name]
+        county_hexgrid_centroids = county_hexgrid.copy()
+        county_hexgrid_centroids['geometry'] = county_hexgrid.centroid
+        start_loc_per_county[str(name)] = county_hexgrid_centroids
+    return start_loc_per_county
+   
 
 
 if __name__ == "__main__":
