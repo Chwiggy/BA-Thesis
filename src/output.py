b6a4b34b8e69f36d10afae6bdd16749d342f0cf5
Emily C Wilke <emilycwilke@gmail.com>
2023-12-17 21:30:36 +0100

    bugfix

--- src/output.py
+++ src/output.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 
 def to_png(name: str, county: str, results: gpd.GeoDataFrame):
-    results.plot(column="mean", cmap="magma_r", legend=True, title=county)
+    results.plot(column="mean", cmap="magma_r", legend=True)
     plt.savefig(f"/home/emily/thesis_BA/data/output/{name}_{county}.png")
     plt.close
 
