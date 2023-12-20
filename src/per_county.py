70eebb81ff0df55a48964e48f93747085cabc708
Emily C Wilke <emilycwilke@gmail.com>
2023-12-20 15:16:57 +0100

    added argparse #21

--- src/per_county.py
+++ src/per_county.py
@@ -3,7 +3,15 @@ import osmfile
 from destination import Destination
 
 def main():
-    # TODO argparse
+    # user input
+    parser = argparse.ArgumentParser(description="all closeness centrality calculations for one county")
+    parser.add_argument("county")
+    parser.add_argument("-g", '--gtfs')
+    args = parser.parse_args()
+    place_name = args.county
+    gtfs_path = args.gtfs
+
+
     place = place_input(place_name) # TODO flesh that out
     gtfs_cropped = crop_gtfs()
     osm_data = osmfile.get_osm_data(geodata=place, name = place_name)
